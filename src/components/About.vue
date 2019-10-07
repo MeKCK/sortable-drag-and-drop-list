f51ff93d4761e9f7023839db66be831c59b078ac
kiprono <42340517+MeKCK@users.noreply.github.com>
2019-10-07 09:13:15 +0300

    first commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>hello {{their_name}}This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+    <p>Lorem ipsum dolor sit amet, ut labore et dolore magna ali...</p>
   </div>
 </template>
 
